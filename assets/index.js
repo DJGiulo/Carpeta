2dc4988bd9ecd70c186d680b26372838a7652ed9
Giulo <giulianolezcano57@gmail.com>
2024-10-04 18:07:59 -0300

    funcion si(if)

---
+++ assets/index.js
@@ -0,0 +1,22 @@
+//const condicion = false
+//const condicion2 = true
+//if (condicion) {
+//    alert ("es verdadero")
+//} else {
+//    alert ("es falso")
+//}
+//else if (condicion2) {
+//    alert ("condicion2 verdadera")
+//}
+
+const num1 = 65;
+
+if (num1 >= 18 && num1 <= 65) {
+    alert ("podes pasar")
+}
+else if (num1 > 65) {
+    alert ("sos mayor")
+}
+else {
+    alert ("sos menor")
+}
