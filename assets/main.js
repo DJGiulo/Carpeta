d091a1aae52c9960c11bca5c99dce8be777a031b
Giulo <giulianolezcano57@gmail.com>
2024-11-15 19:35:49 -0300

    mitadClase9

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,13 @@
-//const condicion = false
-//const condicion2 = true
-//if (condicion) {
-//    alert ("es verdadero")
-//} else {
-//    alert ("es falso")
-//}
-//else if (condicion2) {
-//    alert ("condicion2 verdadera")
-//}
+/*const condicion = false
+const condicion2 = true
+if (condicion) {
+    alert ("es verdadero")
+} else {
+    alert ("es falso")
+}
+else if (condicion2) {
+    alert ("condicion2 verdadera")
+}
 
 const num1 = 65;
 
@@ -19,4 +19,20 @@ else if (num1 > 65) {
 }
 else {
     alert ("sos menor")
+}
+*/
+
+
+
+let otraDD = [
+    [nombre:"asgdiasgdb"];
+    [nombre:"kauhsgdua"];
+    [nombre:"sjkdbhkadb"];
+];
+
+otraDD [1].nombre;
+
+function mostratCategorias(){
+    const categoriasContainer = document.getElementById("categorias");
+    categoriasContainer.innerHTML = baseDeDatos.nap (categoria).join()
 }
